0bbae62072776811e0ab787a724c4873742024a3
chanshunli <chanshunli@gmail.com>
2017-05-12 10:09:13 +0800

    Add org.json/json/20160810

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.json/json "20160810"]]
   :resource-paths ["lib/Msc.jar"]
   :main xunfei-clj.core)
