e00dd0197d4ea92b5705e973266e160a62aabb44
chanshunli <chanshunli@gmail.com>
2017-05-13 22:48:55 +0800

    text-to-vfile: 可生成testest.wav文件,并且输入文本越长则文件越大,不知为什么不能播放

--- src/xunfei_clj/core.clj
+++ src/xunfei_clj/core.clj
@@ -4,7 +4,9 @@
             SpeechConstant
             SpeechUtility
             SpeechSynthesizer
-            SynthesizerListener]
+            SynthesizerListener
+            SynthesizeToUriListener
+            SpeechError]
            [org.json JSONArray JSONObject JSONTokener]
            ))
 
@@ -26,16 +28,40 @@
     )
   )
 
-;; (read-text-as-voice "输入文本,用讯飞语音合成器, 合成发音播放")
-(defn read-text-as-voice
-  [text]
+;; (read-text-as-voice "输入文本,用讯飞语音合成器, 合成发音播放" (fn [mTts text] ...播放或者是保存到音频文件...) )
+(defn read-text-to-voice
+  [text output-fn]
   (let [mTts (SpeechSynthesizer/createSynthesizer)
         _ (.setParameter mTts SpeechConstant/VOICE_NAME "xiaoyan")
         _ (.setParameter mTts SpeechConstant/SPEED "50")
         _ (.setParameter mTts SpeechConstant/VOLUME "80")
-        _ (.setParameter mTts SpeechConstant/TTS_AUDIO_PATH "./tts_test.pcm")
-        ]
+        _ (.setParameter mTts SpeechConstant/TTS_AUDIO_PATH "./tts_test.pcm")]
     ;; Ubuntu: 开始合成, 测试文本,合成读音ok:-)
-    (.startSpeaking mTts text (mSynListenerGen))
+    (output-fn mTts text)
     )
   )
+
+;; (text-to-player "这里是文本播放语音")
+(defn text-to-player
+  [text]
+  (read-text-to-voice
+   text
+   (fn [mTts text] (.startSpeaking mTts text (mSynListenerGen)))))
+
+;; 将text合成的语音保存到文件的合成器
+(defn synthesizeToUriListener
+  []
+  (proxy [SynthesizeToUriListener] []
+    (onBufferProgress [^Integer progress])
+    (onSynthesizeCompleted [^String uri ^SpeechError error])
+    )
+  )
+
+;; (text-to-vfile "将text合成的语音保存到文件" "testest.wav")
+;;todos: 可生成testest.wav文件,并且输入文本越长则文件越大,不知为什么不能播放
+(defn text-to-vfile
+  [text url]
+  (read-text-to-voice
+   text
+   (fn [mTts text]
+     (.synthesizeToUri mTts text url (synthesizeToUriListener)))))
