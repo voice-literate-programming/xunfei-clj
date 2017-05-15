bac9318f3c4ea3d42e6554a28daaddc9c6f1c5be
chanshunli <chanshunli@gmail.com>
2017-05-15 18:09:11 +0800

    Add examples示例

---
+++ examples/hello-xunfei/project.clj
@@ -0,0 +1,14 @@
+(defproject hello-xunfei "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [compojure "1.5.1"]
+                 [ring/ring-defaults "0.2.1"]
+                 [xunfei-clj "0.1.0-SNAPSHOT"]]
+  :resource-paths ["lib/Msc.jar"]
+  :plugins [[lein-ring "0.9.7"]]
+  :ring {:handler hello-xunfei.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring/ring-mock "0.3.0"]]}})
---
+++ examples/hello-xunfei/src/hello_xunfei/handler.clj
@@ -0,0 +1,12 @@
+(ns hello-xunfei.handler
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [xunfei-clj.core :as xunfei]))
+
+(defroutes app-routes
+  (GET "/xunfei/:text" [text] (do (xunfei/text-to-player text) (str "读取成功:" text) ))
+  (route/not-found "Not Found"))
+
+(def app
+  (wrap-defaults app-routes site-defaults))
