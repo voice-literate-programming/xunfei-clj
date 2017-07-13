453d1abf601e008a5fa3177735327f1da41e8e72
Steve Chan <chanshunli@gmail.com>
2017-07-13 14:34:53 +0800

    Update examples

--- examples/hello-xunfei/src/hello_xunfei/handler.clj
+++ examples/hello-xunfei/src/hello_xunfei/handler.clj
@@ -4,6 +4,8 @@
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [xunfei-clj.core :as xunfei]))
 
+(def xunfei-init (xunfei/app-init "your-xunfei-appid"))
+
 (defroutes app-routes
   (GET "/xunfei/:text" [text] (do (xunfei/text-to-player text) (str "读取成功:" text) ))
   (route/not-found "Not Found"))
